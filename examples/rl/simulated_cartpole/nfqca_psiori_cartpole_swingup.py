cb5cf9f10071f43458254065de83c98b8e35ab70
Sascha Lange <sascha@psiori.com>
2025-08-20 10:19:08 +0200

    working on experiment on real cartpole

--- examples/rl/simulated_cartpole/nfqca_psiori_cartpole_swingup.py
+++ examples/rl/simulated_cartpole/nfqca_psiori_cartpole_swingup.py
@@ -307,7 +307,7 @@ for i in range(NUM_EPISODES):
 
 
     try:
-        for iterations in range(1):
+        for iterations in range(100):
             # Fit the controller
             nfqca.fit_critic(
                 batch,
